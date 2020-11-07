2eaf71ab7aab788f0f923e140cb5c95bc46bd3f4
avan4bala <44299374+avan4bala@users.noreply.github.com>
2020-11-07 14:44:24 -0500

    Create HashTable.java

---
+++ CourseDatabase/HashTable.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package asgn4;
+
+import java.util.LinkedList;
+
+/**
+ * @author avanbala
+ *
+ */
+
+public class HashTable
+{
+	
+	LinkedList<CourseDBElement> list;
+	String key;
+
+	public HashTable()
+	{
+		list=new LinkedList<CourseDBElement>();
+		key="";
+	}
+
+
+
+
+}
